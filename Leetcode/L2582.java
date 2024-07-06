f2902868895369deb800201b095f9e248ce263df
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-07-06 19:16:47 -0400

    Update L2582.java

--- Leetcode/L2582.java
+++ Leetcode/L2582.java
@@ -1,14 +1,15 @@
 // Leetcode 2582 - Pass The Pillow 
+// Time Complexity: 𝑂(1); Space Complexity: 𝑂(1)
 
 class Solution {
     public int passThePillow(int n, int time) {
-        int numOfRounds = time / (time - 1);
+        int numOfRounds = time / (n - 1);
         boolean revDirection = (numOfRounds % 2 != 0);
 
-        if(revDirection){
-            return n - (time % (n-1)); 
-        }else{
-            return time % (n-1) + 1;
+        if (revDirection) {
+            return n - (time % (n - 1));
+        } else {
+            return time % (n - 1) + 1;
         }
     }
 }
