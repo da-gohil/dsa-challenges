a9cbee5bb8b886a75029874c4b14fa028fde30e0
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-07-10 11:31:14 -0400

    Update L1701.java

--- Leetcode/L1701.java
+++ Leetcode/L1701.java
@@ -1,25 +1,27 @@
 //L1701 - Restaurant Problem
+// Time Complexity - O(n), Space Complexity - O(1)
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int n = customers.length;
-
-        double totalWaitTime = 0;
-        int currTime = 0;
+        
+        int idleTime = 1;
+        long totalWaitTime = 0;
+        double n = customers.length;
 
         for (int[] customer : customers) {
+        //Chef is available for cooking          
             int arrivalTime = customer[0];
             int cookTime = customer[1];
 
-            if (currTime < arrivalTime) {
-                currTime = arrivalTime;
+            if (idleTime < arrivalTime) {
+                idleTime = arrivalTime + cookTime;
+            }//If chef is preparing order of someone
+            else{
+                idleTime = idleTime + cookTime;
             }
 
-            int waitTime = currTime + cookTime - arrivalTime;
-
-            totalWaitTime += waitTime;
-
-            currTime += cookTime;
+            totalWaitTime = totalWaitTime + (idleTime - arrivalTime);
         }
-        return totalWaitTime / n;
+        double avg = (totalWaitTime / n);
+        return avg;
     }
 }
