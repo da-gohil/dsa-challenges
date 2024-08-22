0af372c79fa1e917a45cd4e82793d5a392a552b8
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-08-22 17:09:56 -0400

    Update L3.java

--- Sliding WIndow Problems/L3.java
+++ Sliding WIndow Problems/L3.java
@@ -1,30 +1,25 @@
-// Leetcode 03 - Sliding WIndow Problem
-import java.util.ArrayList;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int start = 0;
-        int end = 0;
+        int windowStart = 0;
+        int windowEnd = 0;
         int maxLength = 0;
 
-        ArrayList<Character> list = new ArrayList<Character>();
+        HashSet<Character> set = new HashSet<>();
 
-        while (end < s.length()) {
-            // If list doesn't contain that character then only we add that character
-            if (!list.contains(s.charAt(end))) {
-                list.add(s.charAt(end));
-                end++;
-                maxLength = Math.max(maxLength, list.size());
+        while (windowEnd < s.length()) {
+            // If set doesn't contain that character, add it to the set and update maxLength
+            if (!set.contains(s.charAt(windowEnd))) {
+                set.add(s.charAt(windowEnd));
+                windowEnd++;
+                maxLength = Math.max(maxLength, set.size());
             }   
-            // If we already have that character then we need to remove it
+            // If the set already contains the character, remove the character at windowStart
             else {
-                // If we do not write valueOf, we will get ASCII value of the character and error will be thrown
-                list.remove(Character.valueOf(s.charAt(start)));
-                // After removing, we will update the start to one ahead of the window
-                start++;
+                set.remove(s.charAt(windowStart));
+                windowStart++;
             }
         }
-
         return maxLength;
     }
 }
