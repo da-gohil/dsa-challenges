da910998991afb89ab7b1150bcf554161f300a07
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-08-26 14:08:32 -0400

    Create L150_Postfix.java

---
+++ Stack/L150_Postfix.java
@@ -0,0 +1,40 @@
+//Leetcode 150 - Reverse Polish Notation
+
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        
+        // Iterate through each token
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                // Pop two operands from the stack
+                int b = stack.pop();
+                int a = stack.pop();
+                
+                // Perform the operation and push the result back onto the stack
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b); // Integer division
+                        break;
+                }
+            } else {
+                // Token is an operand, push it onto the stack
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        // The result is the only element left in the stack
+        return stack.pop();
+    }
+}
