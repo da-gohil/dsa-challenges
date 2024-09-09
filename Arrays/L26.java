d19b7bab7f1d01546d01308e99e5845eedc62c8c
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-09-09 12:29:18 -0400

    Update L26.java

--- Arrays/L26.java
+++ Arrays/L26.java
@@ -3,9 +3,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-        int count =0;
-        for(int i=0; i<nums.length; i++){
-            if(o < nums.length -1 || nums[i] ==  nums[i+1]){
+        int count = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(i < nums.length -1 || nums[i] ==  nums[i+1]){
                 continue;
             }else{  
                 nums[count] = nums[i];
@@ -14,5 +14,5 @@ class Solution {
         }
         return count;
     }
-    
+
 }
