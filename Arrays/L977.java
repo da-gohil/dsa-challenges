571b11aa4375af8f296a16a931838bc048db0e25
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-09-10 14:53:19 -0400

    Update L977.java

--- Arrays/L977.java
+++ Arrays/L977.java
@@ -1,45 +1,14 @@
-// Leetcode 971
-// Approach 01 - Using Fixed size - Arrays
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         
-        // Calculate the squares of each number
-        for (int i = 0; i < n; i++) {
-            int square = nums[i] * nums[i];
-            res[i] = square;
+        for(int i =0; i<nums.length; i++){
+            int squared = nums[i] * nums[i];
+            res[i] = squared;
         }
-        
-        // Sort the squared numbers
         Arrays.sort(res);
-        
         return res;
-    }
-}
-// Approach 02 - Using Dynamic size - ArrayList
-import java.util.ArrayList;
-import java.util.Collections;
-
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        ArrayList<Integer> squaredList = new ArrayList<>();
-        
-        // Calculate the squares of each number
-        for (int number : nums) {
-            int square = number * number;
-            squaredList.add(square);
-        }
-        
-        // Sort the list
-        Collections.sort(squaredList);
-        
-        // Convert ArrayList to int[]
-        int[] squaredArray = new int[squaredList.size()];
-        for (int i = 0; i < squaredList.size(); i++) {
-            squaredArray[i] = squaredList.get(i);
-        }
-        
-        return squaredArray;
     }
 }
