f16f4c5940a7d1b5fa2c484938d3317497216ba5
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-09-10 14:53:12 -0400

    Create L448.java

---
+++ Arrays/L448.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+//Attempt 02
+import java.util.*;
+
+class L448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> missingNumbers = new ArrayList<>();
+        
+        // Mark presence
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        
+        // Find missing numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missingNumbers.add(i + 1);
+            }
+        }
+        
+        return missingNumbers;
+    }
+
+    public static void main(String[] args) {
+        L448 sol = new L448();
+        int[] nums = {1, 2, 2, 4};
+        System.out.println(sol.findDisappearedNumbers(nums)); // Output: [3]
+    }
+}
+
+
+// Attempt 01
+// class L448 {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         List<Integer> missingNumbers = new ArrayList<>();
+//         Arrays.sort(nums);
+//         int range = nums.length;
+
+//         for(int i=1; i<=nums.length; i++){
+//             missingNumbers.add(i);
+//         }
+//         for(int i=0; i<nums.length;i++){
+//             if(nums[i]!=missingNumbers.get(i)){
+//                 missingNumbers.remove(i);
+//             }
+//         }
+//         return missingNumbers;
+//     }
+//     public static void main(String []args){
+//         int[] nums = {1,2,2,4};
+//         L448 sol = new L448();
+//         System.out.println(sol.findDisappearedNumbers(nums));
+//     }
+// }
