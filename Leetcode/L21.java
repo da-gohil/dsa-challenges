6d200709f54ec2aeca6cfeaef084828173bd73b1
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-09-23 22:22:48 -0400

    Create L21.java

---
+++ Leetcode/L21.java
@@ -0,0 +1,31 @@
+//Leetcode 21 - Merge Two sorted list
+//LinkedIN List problem and not Array
+
+import java.util.List;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode tempNode = new ListNode(0);
+        ListNode currentNode = tempNode;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                currentNode.next = list1;
+                list1 = list1.next;
+            }else{
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+        if(list1 != null){
+            currentNode.next = list1;
+            list1 = list1.next;
+        }
+        if(list2 != null){
+            currentNode.next = list2;
+            list2 = list2.next;
+        }
+        return tempNode.next;
+    }
+}
