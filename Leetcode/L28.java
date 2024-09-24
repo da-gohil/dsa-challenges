4d4eda081e793089a6be04f3e7f430ebc07e65b2
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-09-23 23:41:17 -0400

    Update L28.java

--- Leetcode/L28.java
+++ Leetcode/L28.java
@@ -2,14 +2,20 @@
 
 class Solution {
     public int strStr(String haystack, String needle) {
-        
-        for(int i=0; i<haystack.length()-needle.length()+1; i++){
-            if(haystack.charAt(i) == needle.charAt(i)){
-                if(haystack.substring(i, needle.length()+1).equals(needle)){
-                    return i;
-                }
+        // Edge case: If needle is an empty string, return 0
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        // Loop through the haystack, but stop when the remaining length is less than needle length
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            // Check if the substring from the current index matches the needle
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i; // Return the index of the first occurrence
             }
         }
+
+        // If no match is found, return -1
         return -1;
     }
 }
