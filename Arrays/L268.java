675eead7930228e63c4652ff85bf454b89983fad
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2025-09-21 12:59:39 -0400

    Update L268.java

--- Arrays/L268.java
+++ Arrays/L268.java
@@ -1,26 +1,27 @@
-class Solution {
+class L268{
     public int missingNumber(int[] nums) {
-        if(nums.length == 0 || nums == null){
-            return 0;
+        if (nums == null) {
+            // Handle the case where the input array reference is null
+            throw new IllegalArgumentException("Input array cannot be null.");
         }
 
         int n = nums.length;
-        int total = n * (n + 1) / 2;
-        int sum = 0;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
 
-        for(int num : nums){
-            sum = sum + num;
+        for (int num : nums) {
+            actualSum += num;
         }
-        return total - sum;
+
+        return expectedSum - actualSum;
     }
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        int [] numbers = {1,0,3};
+        L268 sol = new L268();
+
+        int[] numbers = {2};
         int result = sol.missingNumber(numbers);
 
-        System.out.println("Missing Number in the list is: " + res);
-
+        System.out.println("Missing Number in the list is: " + result);
     }
 }
