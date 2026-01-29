73a7cb79d0d6587c183e73f3d2cc63bf0b630690
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-01-29 16:52:00 -0500

    Update LC01TwoSum.java

--- Uber Top 52/LC01TwoSum.java
+++ Uber Top 52/LC01TwoSum.java
@@ -1,6 +1,18 @@
-
-public class LC01TwoSum {
-    public int[] twoSum(int [] nums, int target){
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         
+        Map<Integer, Integer> resultMap = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int reqNumber = target - nums[i];
+
+            if(resultMap.containsKey(reqNumber)){
+                int [] arr = {resultMap.get(reqNumber), i};
+                return arr;
+            }
+            resultMap.put(nums[i], i);
+        }
+        
+        return null;
     }
 }
